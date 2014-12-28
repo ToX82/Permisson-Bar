8d9034eb7e319538e602bb2a4886f7e5b5a0db7f
tox82 <toss82@gmail.com>
2014-12-28 14:12:04 +0100

    Code cleaning, full multilanguage support, bar is shown depending only if cookies or localStorage are used

--- README.md
+++ README.md
@@ -13,14 +13,16 @@ Permission bar makes it simple and clear to visitors that cookies are in use and
 
 ## Important Legal Reading
 
-1. Information Commissioner's Office - [Guidance on the rules on use of cookies and similar technologies (PDF)](http://www.ico.gov.uk/news/latest_news/2011/~/media/documents/library/Privacy_and_electronic/Practical_application/guidance_on_the_new_cookies_regulations.ashx)
+1. [Read the EU legislation on cookies](http://ec.europa.eu/ipg/basics/legal/cookies/index_en.htm)
 
 
 ## How it works?
  
-Permisson Bar uses jQuery for its animations and events. It checks if 'jQuery' is defined, and if it's not, it provides the library by itself (version 1.7.2 - from Google's CDN). 
+Permisson Bar uses jQuery for its animations and events. It checks if 'jQuery' is defined, and if it's not, it provides the library by itself (version 1.8 - from Google's CDN). 
 
-Once user clicks 'Allow Cookies' Permission Bar will set a cookie for that domain with a name 'permissionbar' that will expire in 365 days. This means that the plugin will only show up once per domain (per year). 
+Once user clicks 'Allow Cookies' Permission Bar will set a cookie for that domain with a name 'permissionbar' that will expire in 30 days. This means that the plugin will only show up once per domain (per month). 
+
+If a user decides to click "Disallow Cookies", Permissions Bar will simply remove all cookies and localStorage data.
 
 
 ## How to Install?
@@ -29,12 +31,17 @@ Once user clicks 'Allow Cookies' Permission Bar will set a cookie for that domai
 2. Put the following code on all relevant pages of your website right before the &lt;/body&gt; tag.:
 
 	<code>
-	&lt;script type="text/javascript" src="PATH-TO/permissionbar-latest.min.js"&gt;&lt;/script&gt;
+	&lt;script type="text/javascript" src="PATH-TO/permissionbar-latest.js"&gt;&lt;/script&gt;
 	</code>
 	
 ## Changelog
 
-### Version 1.5 
+### Version 1.6
+
+	Version 1.6
+	Date of Commit 28.12.14
+	Forked by Emanuele "ToX" Toscano, added multilanguage (browser detected) and localStorage support. Added some additional checks to verify if
+	the bar is needed or not (there is no reason to show this bar if you are not using cookies nor localStorage). Several other minor changes too.
 
 	Version 1.5
 	Date of Commit 28.05.12
--- permissionbar-latest.js
+++ permissionbar-latest.js
@@ -7,52 +7,49 @@
 */
 var jQload = false;
 var languages = [
-	'en',
-	'it',
-	'fr'
+    'en',
+    'it',
+    'fr'
 ];
 
-function initPermBar() {
+ function setupPermissionsBar() {
     if (typeof (jQuery) == 'undefined') {
-        if (!jQload) {
+        if(!jQload) {
             jQload = true;
-            document.write(unescape('%3Cscript type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"%3E%3C/script%3E'));
+            var script = document.createElement('script');
+            script.type = "text/javascript";
+            script.src = "//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js";
+            document.getElementsByTagName('head')[0].appendChild(script);
         }
-        setTimeout("initPermBar()", 50);
-    } else {;
+        setTimeout(setupPermissionsBar, 50);
+    } else {
         (function ($) {
-            var loadExternal = function loadExternal(filename, filetype) {
-                if (filetype == "js") {
-                    var fileref = document.createElement('script');
-                    fileref.setAttribute("type", "text/javascript");
-                    fileref.setAttribute("src", filename);
-                } else if (filetype == "css") {
-                    var fileref = document.createElement("link");
-                    fileref.setAttribute("rel", "stylesheet");
-                    fileref.setAttribute("type", "text/css");
-                    fileref.setAttribute("href", filename);
-                }
-                if (typeof fileref != "undefined") {
-                    document.getElementsByTagName("head")[0].appendChild(fileref);
-                }
-            };
-            var theSetup = function thesetup() {
-                loadExternal("permissionbar.css", "css");
+            function loadAssets() {
+                // Load CSS file
+                var fileref = document.createElement("link");
+                fileref.setAttribute("rel", "stylesheet");
+                fileref.setAttribute("type", "text/css");
+                fileref.setAttribute("href", "permissionbar.css");
+                document.getElementsByTagName("head")[0].appendChild(fileref);
 
                 // Get browser's language
-				var userLang = navigator.language || navigator.userLanguage;
+                var userLang = navigator.language || navigator.userLanguage;
+                // Specify userLang = "xx" here if you need to force a specific language
 
-				if ( $.inArray(userLang, languages) < 0) {
-					userLang = "en";
-				}
-				$.ajax({
-					url: "lang/" + userLang + ".html",
-					async: false
-				}).done(function( html ) {
-					$('body').append(html);
-				});
-            };
-            var getCookie = function getCookie(c_name) {
+                if ( $.inArray(userLang, languages) < 0) {
+                    userLang = "en";
+                }
+
+                // Load the correct language messages file
+                $.ajax({
+                    url: "lang/" + userLang + ".html",
+                    async: false
+                }).done(function( html ) {
+                    $('body').append(html);
+                });
+            }
+
+            function getCookie(c_name) {
                 var i, x, y, ARRcookies = document.cookie.split(";");
                 for (i = 0; i < ARRcookies.length; i++) {
                     x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
@@ -62,67 +59,67 @@ function initPermBar() {
                         return unescape(y);
                     }
                 }
-            };
-            var setCookie = function setCookie(c_name, value, exdays) {
+            }
+            function setCookie(c_name, value, exdays) {
                 var exdate = new Date();
                 exdate.setDate(exdate.getDate() + exdays);
                 var c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString());
                 document.cookie = c_name + "=" + c_value;
-            };
-            var checkCookie = function checkCookie() {
+            }
+            function checkCookie() {
                 var permissionbar = getCookie("permissionbar");
                 if (permissionbar === null || permissionbar === "" || permissionbar === undefined) {
-
-                    $('#permission-bar').fadeToggle('slow');
-                    var button = $('#permission-bar-button');
-                    button.on('click', function (e) {
-                        setCookie("permissionbar", "CookiesAllowed", 365);
-                        $('#permission-bar').fadeToggle('slow');
-                    });
+                    $('#permission-bar').fadeIn('slow');
                 }
-            };
-            var removeCookies = function removeCookies() {
-            	// Clear cookies
-			    document.cookie.split(";").forEach(function(c) { 
-			    	document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
-			    });
-			    // Clear localStorage
-			    localStorage.clear();
-            };
-            var permissionBarInit = function permissionBarInit() {
+            }
+            function removeCookies() {
+                // Clear cookies
+                document.cookie.split(";").forEach(function(c) { 
+                    document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+                });
+                // Clear localStorage
+                localStorage.clear();
+            }
+
+            function actions() {
                 var button = $('#permission-bar-button');
                 var buttonNo = $('#permission-bar-button-no');
                 var promptBtn = $('#permission-bar-prompt-button');
                 var promptClose = $('#permission-bar-prompt-close');
                 var prompt = $('#permission-bar-prompt');
                 var promptContent = $('#permission-bar-prompt-content');
-                promptBtn.on('click', function (e) {
-                    prompt.fadeToggle('slow');
-                });
-                promptClose.on('click', function (e) {
-                    prompt.fadeToggle('slow');
-                });
-                promptContent.css({
-                    top: $(window)
-                        .height() / 2 - 225,
-                    left: ($(window)
-                        .width() / 2) - 225
+
+                button.on('click', function () {
+                    setCookie("permissionbar", "CookiesAllowed", 30);
+                    $('#permission-bar').fadeOut('slow');
                 });
                 buttonNo.on('click', function() {
-                	removeCookies();
-                	$('#permission-bar').fadeOut('slow');
+                    removeCookies();
+                    $('#permission-bar').fadeOut('slow');
                 });
-            };
-            var permissionBar = function permissionBar() {
-                theSetup();
-                permissionBarInit();
-                checkCookie();
-            };
-            
+
+                promptBtn.on('click', function () {
+                    prompt.fadeIn('slow');
+                });
+                promptClose.on('click', function () {
+                    prompt.fadeOut('slow');
+                });
+                promptContent.css({
+                    top: $(window).height() / 2 - 225,
+                    left: ($(window).width() / 2) - 225
+                });
+            }
+
             $(document).ready(function () {
-                permissionBar();
+                loadAssets();
+                actions();
+                checkCookie();
             });
         })(jQuery);
     }
 }
-initPermBar();
+
+// Load the script only if there is at least a cookie or a localStorage item
+if (document.cookie.length > 0 || window.localStorage.length > 0) {
+    setupPermissionsBar();
+}
